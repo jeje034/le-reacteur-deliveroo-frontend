a99bf8453882edeb86cee418e2deb349d375f19f
Jeje S <54934028+jeje034@users.noreply.github.com>
2021-02-09 19:36:01 +0100

    Livraison partielle CSS panier

--- src/Basket.js
+++ src/Basket.js
@@ -1,3 +1,6 @@
+import lessPicture from "./assets/Less.PNG";
+import plusPicture from "./assets/Plus.PNG";
+
 const Basket = ({ basket, setBasket, updateBasket }) => {
     const handleClick = (meal, quantityToAdd) => {
         updateBasket(meal.id, meal.title, quantityToAdd, meal.price);
@@ -7,30 +10,41 @@ const Basket = ({ basket, setBasket, updateBasket }) => {
 
     return (
         <div className="basket-card">
-            <button>Valider votre panier</button>
+            <button className="basket-validation">Valider mon panier</button>
 
             {basket && basket.length > 0 && basket[0].title ? (
                 basket.map((meal, indice) => {
                     subtotal += meal.quantity * meal.price;
                     return (
                         <div className="basket-meal" key={meal.id}>
-                            <button
+                            <img
+                                className="basket-plus-less-button basket-less-button"
                                 onClick={() => {
                                     handleClick(meal, -1);
                                 }}
-                            >
-                                -
-                            </button>
-                            <p>{meal.quantity}</p>
-                            <button
-                                onClick={() => {
-                                    handleClick(meal, 1);
-                                }}
-                            >
-                                +
-                            </button>
-                            <div key={meal.id}>
-                                {" " + meal.title + " " + meal.price}
+                                src={lessPicture}
+                                alt="Moins"
+                            ></img>
+                            <p className="basket-quantity-for-1-meal">
+                                {meal.quantity}
+                            </p>
+                            {
+                                <img
+                                    className="basket-plus-less-button"
+                                    onClick={() => {
+                                        handleClick(meal, 1);
+                                    }}
+                                    src={plusPicture}
+                                    alt="Plus"
+                                ></img>
+                            }
+                            <div className="basket-title-and-price">
+                                <div className="basket-meal-title">
+                                    {meal.title}
+                                </div>
+                                <div className="basket-amount-for-1-meal">
+                                    {meal.price + " €"}
+                                </div>
                             </div>
                         </div>
                     );
@@ -38,9 +52,22 @@ const Basket = ({ basket, setBasket, updateBasket }) => {
             ) : (
                 <div>"Votre panier est vide"</div>
             )}
-            {subtotal > 0 && <div>{"Sous-total : " + subtotal}</div>}
-            {subtotal > 0 && <div>{"Frais de livraison : 2.50 €"}</div>}
-            {subtotal > 0 && <div>{"Total : " + (subtotal + 2.5)}</div>}
+            {subtotal > 0 && (
+                <div>
+                    <div className="basket-sums">
+                        <div>Sous-total</div>
+                        <div>{subtotal + " €"}</div>
+                    </div>
+                    <div className="basket-sums">
+                        <div>Frais de livraison</div>
+                        <div>2.50 €</div>
+                    </div>
+                    <div className="basket-sums">
+                        <div>Total</div>
+                        <div>{subtotal + 2.5 + " €"}</div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
